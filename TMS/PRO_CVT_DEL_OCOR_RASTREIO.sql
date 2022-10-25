4fb0cb3b10aabc94e2893607dcc083e63add75ea
Eduardo Teixeira <34588048+EDUARDO-TEIXEIRA@users.noreply.github.com>
2022-10-25 14:26:38 -0300

    Update PRO_CVT_DEL_OCOR_RASTREIO.sql

--- TMS/PRO_CVT_DEL_OCOR_RASTREIO.sql
+++ TMS/PRO_CVT_DEL_OCOR_RASTREIO.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE "PRO_CVT_DEL_OCOR_RASTREIO" (
+create or replace PROCEDURE "PRO_CVT_DEL_OCOR_RASTREIO" (
        P_CODUSU NUMBER,       
        P_IDSESSAO VARCHAR2,   
        P_QTDLINHAS NUMBER,    
@@ -13,37 +13,34 @@ CREATE OR REPLACE PROCEDURE "PRO_CVT_DEL_OCOR_RASTREIO" (
 
 BEGIN       
        FIELD_CHNFE          := ACT_TXT_FIELD(P_IDSESSAO,1,  'CHNFE');
+
+
        FIELD_TRANSPORTADORA := ACT_TXT_FIELD(P_IDSESSAO,1,  'TRANSPORTADORA');
 
        V_ALTERAR :=  ACT_CONFIRMAR('Exclusão de registros', 
                                    'Esta operação é irreverssível, deseja continuar?', P_IDSESSAO, 1);
-                                   
+
        IF P_QTDLINHAS = 0 THEN
           RAISE_APPLICATION_ERROR(-20000, FC_FORMATAHTML(NULL,'Selecione uma ocorrência a ser excluída.', NULL));       
        END IF; 
-       
+
        FOR I IN 1..P_QTDLINHAS LOOP    
        FIELD_SEQOCOR        := ACT_INT_FIELD(P_IDSESSAO, I,  'SEQOCOR');
        FIELD_CODOCORRENCIA  := ACT_INT_FIELD(P_IDSESSAO, I,  'CODOCORRENCIA');
 
-           SELECT COUNT(1)
-             INTO V_VERIFICAOCOR
-             FROM AD_ACENTREGA
-            WHERE CHNFE = FIELD_CHNFE
-              AND NVL(CODOCORRENCIA,0) != 9999;
-           
-           IF V_VERIFICAOCOR > 0 THEN
+
+           IF NVL(FIELD_CODOCORRENCIA, '9999') != 9999 THEN
               RAISE_APPLICATION_ERROR(-20000, 
                                       FC_FORMATAHTML('Alteração de Registro - Operação não permitida',
                                       'o rastreio é automático realizado pela transportadora '|| FIELD_TRANSPORTADORA|| '.', 
                                       NULL));
            END IF;
-           
+
            DELETE FROM AD_ACENTREGA 
                  WHERE CHNFE = FIELD_CHNFE 
                    AND SEQOCOR = FIELD_SEQOCOR;
-                   
+
        END LOOP; 
-       
-       P_MENSAGEM:='Registro atualizado com sucesso '|| FIELD_SEQOCOR|| ', chave: ' || FIELD_CHNFE ;  
+
+       P_MENSAGEM:='Registros removidos';  
 END;
